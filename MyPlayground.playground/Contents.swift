ed61e2dbb1819c55b1c1ed3fae2b08454dc1aeb9
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-01-28 22:18:31 -0800

    SwiftApp: Install Xcode & Using

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+let helloworld = "Hello, World"
+
+print(helloworld)
---
+++ SingleView.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.backgroundColor = .blue
+        })
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
